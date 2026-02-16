0b4c83fe7378ea68efe48c89612cff7678cd5bd0
Anshu-WebD <anshuweb@bigwigmediadigital.com>
2026-02-16 12:01:58 +0530

    Product and coleection API Working

--- config/database.js
+++ config/database.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
-const connect=()=>{
-    const MongoURL= process.env.MongoURL;
-    const DB =process.env.DB;
-    mongoose
+const connect = () => {
+  const MongoURL = process.env.MongoURL;
+  const DB = process.env.DB;
+  mongoose
     .connect(`${MongoURL}/${DB}`, {})
-    .then(()=> console.log("Database connected Successfully"))
-    .catch((reason)=>{
-        console.log(`Unable to connect Databse \n${reason}`);
+    .then(() => console.log("Database connected Successfully"))
+    .catch((reason) => {
+      console.log(`Unable to connect Databse \n${reason}`);
     });
-}
+};
 
-module.exports={connect};
+module.exports = { connect };
--- models/product.model.js
+++ models/product.model.js
@@ -10,7 +10,7 @@ const ProductSchema = new mongoose.Schema(
 
     price: { type: Number },
 
-    collection: {
+    collectionName: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Collection",
       required: true,
